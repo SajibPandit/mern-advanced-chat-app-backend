bdfac069119a6d4e6984f2cc0ff0e16811eb22f2
Sajib Pandit <sajibpandit1999@gmail.com>
2022-05-20 17:59:01 +0600

    dummy api created

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const express = require('express');
+const express = require("express");
+const { chats } = require("./data/data");
+require('dotenv').config()
 
 const app = express();
 
-app.listen(5000,()=>console.log(`Server started on port 5000`));
+app.get("/", (req, res) => {
+  res.send("API is runnig");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:chatId", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.chatId);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
